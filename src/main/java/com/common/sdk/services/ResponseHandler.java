7f6a358f6e85a67f00f3469d40c0914767011186
backendverse <225637829+backendverse@users.noreply.github.com>
2025-11-22 12:30:57 +0530

    ResponseHandler Implemented

--- src/main/java/com/common/sdk/services/ResponseHandler.java
+++ src/main/java/com/common/sdk/services/ResponseHandler.java
@@ -1,10 +1,42 @@
 package com.common.sdk.services;
 
+import com.common.sdk.models.dto.ApiResponse;
+import com.common.sdk.models.enums.APIResponseCode;
+import com.common.sdk.models.interfaces.GenericAPIResponse;
+import com.common.sdk.models.interfaces.ResponseCode;
 import org.springframework.stereotype.Component;
+
+import static com.common.sdk.utils.CommonUtils.getCurrentRequestId;
 
 @Component
 public class ResponseHandler {
 
+    public <T> GenericAPIResponse<T> ok(T data) {
+        return ApiResponse.success(data, APIResponseCode.SUCCESS, getCurrentRequestId());
+    }
 
+    public <T> GenericAPIResponse<T> ok() {
+        return ApiResponse.success(APIResponseCode.SUCCESS, getCurrentRequestId());
+    }
+
+    public <T> GenericAPIResponse<T> ok(T data, ResponseCode responseCode) {
+        return ApiResponse.success(data, responseCode, getCurrentRequestId());
+    }
+
+    public GenericAPIResponse<Void> ok(ResponseCode responseCode) {
+        return ApiResponse.success(responseCode, getCurrentRequestId());
+    }
+
+    public GenericAPIResponse<Void> failure(ResponseCode responseCode) {
+        return ApiResponse.failure(responseCode, getCurrentRequestId());
+    }
+
+    public GenericAPIResponse<Void> failure(int code, String message) {
+        return ApiResponse.failure(code, message, getCurrentRequestId());
+    }
+
+    public GenericAPIResponse<Void> failure(ResponseCode responseCode, String message) {
+        return ApiResponse.failure(responseCode.getCode(), message, getCurrentRequestId());
+    }
 
 }
--- src/main/java/com/common/sdk/utils/CommonUtils.java
+++ src/main/java/com/common/sdk/utils/CommonUtils.java
@@ -1,7 +1,15 @@
 package com.common.sdk.utils;
 
+import org.slf4j.MDC;
+
+import static com.common.sdk.constants.ApplicationConstant.CORRELATION_ID;
+
 public final class CommonUtils {
 
     private CommonUtils() {
+    }
+
+    public static String getCurrentRequestId() {
+        return MDC.get(CORRELATION_ID);
     }
 }
